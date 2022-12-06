01278417adc15ac00c3c24ef0b51ad176ba6c31b
alperozdamar <alper.ozdamar@gmail.com>
2022-12-06 13:24:21 -0500

    Parse HTTP JSON Response

--- main.go
+++ main.go
@@ -1,1 +1,42 @@
-package go_aws_course
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+)
+
+func main() {
+
+	args := os.Args
+
+	if len(args) < 2 {
+		fmt.Println("Usage: ./hello-world <argument> \n")
+		os.Exit(1)
+	}
+
+	if _, err := url.ParseRequestURI(args[1]); err != nil {
+		fmt.Println(" URL is in invalid format: %s\n", err)
+		os.Exit(1)
+	}
+
+	response, err := http.Get(args[1])
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer response.Body.Close()
+
+	body, err := io.ReadAll(response.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("HTTP Status Code: %d\nBody: %s\n", response.StatusCode, string(body))
+
+}
