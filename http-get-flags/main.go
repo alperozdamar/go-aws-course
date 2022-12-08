91a5d3653aa6ae1fc9e858d233ec8a23ca0af914
alperozdamar <alper.ozdamar@gmail.com>
2022-12-08 00:33:42 -0500

    Flag Package

---
+++ http-get-flags/main.go
@@ -0,0 +1,146 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+)
+
+type Response interface {
+	GetResponse() string
+}
+
+type Page struct {
+	Name string `json:"page"`
+}
+
+type Words struct {
+	Input string   `json:"input"`
+	Words []string `json:"words"`
+}
+
+func (w Words) GetResponse() string {
+	return fmt.Sprintf("Words: %s", strings.Join(w.Words, ", "))
+}
+
+type Occurrence struct {
+	Words map[string]int `json:"words"`
+}
+
+func (o Occurrence) GetResponse() string {
+	words := []string{}
+	for word, occurrence := range o.Words {
+		words = append(words, fmt.Sprintf("%s (%d)", word, occurrence))
+	}
+	return fmt.Sprintf("Words: %s", strings.Join(words, ", "))
+}
+
+func main() {
+
+	var (
+		requestURL string
+		password   string
+		parsedURL  *url.URL
+		err        error
+	)
+
+	flag.StringVar(&requestURL, "url", "", "url to access")
+	flag.StringVar(&password, "password", "", "password to access our api")
+	flag.Parse()
+
+	if parsedURL, err = url.ParseRequestURI(requestURL); err != nil {
+		fmt.Printf("Validation Error: URL is not valid: %s \nUsage: ./http-get -h\n", err)
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	args := os.Args
+
+	if len(args) < 2 {
+		fmt.Printf("Usage: ./http-get <url>\n")
+		os.Exit(1)
+	}
+
+	res, err := doRequest(parsedURL.String())
+	if err != nil {
+		if requestErr, ok := err.(RequestError); ok {
+			fmt.Printf("Error: %s (HTTP Code: %d, Body: %s)\n", requestErr.Err, requestErr.HTTPCode, requestErr.Body)
+			os.Exit(1)
+		}
+		fmt.Printf("Error occurred: %s\n", err)
+		os.Exit(1)
+	}
+	if res == nil {
+		fmt.Printf("No response\n")
+		os.Exit(1)
+	}
+	fmt.Printf("Response: %s\n", res.GetResponse())
+
+}
+
+func doRequest(requestURL string) (Response, error) {
+	response, err := http.Get(requestURL)
+	if err != nil {
+		return nil, fmt.Errorf("Get error: %s", err)
+	}
+
+	defer response.Body.Close()
+	body, err := io.ReadAll(response.Body)
+
+	if err != nil {
+		return nil, fmt.Errorf("ReadAll error: %s", err)
+	}
+
+	if response.StatusCode != 200 {
+		return nil, fmt.Errorf("Invalid output (HTTP Code %d): %s\n", response.StatusCode, string(body))
+	}
+
+	if !json.Valid(body) {
+		return nil, RequestError{
+			HTTPCode: response.StatusCode,
+			Body:     string(body),
+			Err:      fmt.Sprintf("No valid JSON returned"),
+		}
+	}
+
+	var page Page
+	err = json.Unmarshal(body, &page)
+	if err != nil {
+		return nil, RequestError{
+			HTTPCode: response.StatusCode,
+			Body:     string(body),
+			Err:      fmt.Sprintf("Page Unmarshall error : %s", err),
+		}
+	}
+	switch page.Name {
+	case "words":
+		var words Words
+		err = json.Unmarshal(body, &words)
+		if err != nil {
+			return nil, RequestError{
+				HTTPCode: response.StatusCode,
+				Body:     string(body),
+				Err:      fmt.Sprintf("Words Unmarshall error : %s", err),
+			}
+		}
+		return words, nil
+	case "occurrence":
+		var occurrence Occurrence
+		err = json.Unmarshal(body, &occurrence)
+		if err != nil {
+			return nil, RequestError{
+				HTTPCode: response.StatusCode,
+				Body:     string(body),
+				Err:      fmt.Sprintf("Occurrence Unmarshall error : %s", err),
+			}
+		}
+
+		return occurrence, nil
+	}
+	return nil, nil
+}
